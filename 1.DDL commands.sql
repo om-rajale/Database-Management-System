5166107a72e33bf2bacd9c3f4536e0127aaf6a7a
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2024-11-24 00:49:41 +0530

    Create 1.create and alter the table DDL .sql

---
+++ 1.DDL commands.sql
@@ -0,0 +1,20 @@
+--to see the available databases
+show databases;
+--to create database
+create database CSE;
+--using the created database
+use CSE;
+-- now it will uses database cse and perform operations in it
+--creating table and giving name of columns with datatype (schema)
+create table student( roll_no int(3), Name varchar(20), City varchar(20));
+--to describe  the schema of table
+describe student;
+--updating table
+--for adding constraints
+alter table student add age int(3);
+--updating values
+alter table student modify Name varchar(25);
+--changing arrangement
+alter table student modify age int(3) after Name;
+--changing the name of table 
+rename table table student to scholer;
