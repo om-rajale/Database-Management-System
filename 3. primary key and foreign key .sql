55f6cd7d0653c431efe49c2d7b8e2739d284e966
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2024-11-26 00:25:35 +0530

    Create 3. primary key and foreign key .sql

---
+++ 3. primary key and foreign key .sql
@@ -0,0 +1,8 @@
+--primary key is like unique but always not null
+--FOREIGN KEY
+-- to use foreign key we need to create two table
+--1.PARENT TABLE
+create table dept( d_no int(3) , d_name varchar(25);
+--2.CHILD TABLE
+create table emp( e_no int(3), e_name varchar(25), salary int(6) , d_no int(3), foreign key (d_no) references dept(d_no) on delete cascade);
+--now value of d_no will be deleted if we delete d_no from parent table
