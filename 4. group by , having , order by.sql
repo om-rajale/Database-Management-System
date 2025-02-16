9b194474b7012cf8ca9178114dcc672d1a31e705
Om Rajale <163680131+om-rajale@users.noreply.github.com>
2025-02-16 17:48:19 +0530

    Update 4. group by , having , order by.sql

--- 4. group by , having , order by.sql
+++ 4. group by , having , order by.sql
@@ -5,7 +5,7 @@ insert into company values(112 , 'Microsoft', 500000),
     -> (114, 'Google',950000),
     -> (114, 'Google',850000),
     -> (113, 'Intel',600000), 
-    -> (113, 'Intel',370000),
+    -> (113, 'Intel',370000),        
     -> (112, 'Microsoft',950000); 
 select company_name, sum(salary) from group by company_name;
 --2. HAVING Clause
